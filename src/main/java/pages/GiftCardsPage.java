937a346590d01ddbad0f1bcead432c85192d0916
Himanshu Raj <himanshuraj9194@gmail.com>
2022-03-25 12:38:52 +0530

    added payment page verification page

--- src/main/java/pages/GiftCardsPage.java
+++ src/main/java/pages/GiftCardsPage.java
@@ -1,6 +1,7 @@
 package pages;
 
 import java.awt.Window;
+import java.time.Duration;
 import java.util.List;
 import java.util.Properties;
 
@@ -9,6 +10,8 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 
 import com.aventstack.extentreports.ExtentTest;
@@ -17,42 +20,46 @@ import com.aventstack.extentreports.Status;
 import BaseClasses.PagesBaseClass;
 import utilities.ReadPropertiesFile;
 
-public class GiftCardsPage extends PagesBaseClass{
+public class GiftCardsPage extends PagesBaseClass {
 	Properties prop;
-	public GiftCardsPage(WebDriver ldriver,ExtentTest logger) {
+
+	public GiftCardsPage(WebDriver ldriver, ExtentTest logger) {
 		super(ldriver, logger);
 		ReadPropertiesFile objProp = new ReadPropertiesFile();
 		prop = objProp.readPropertiesFile();
-		
+
 	}
-	
+
 	@FindBy(xpath = "//p[contains(text(),'Corporate Gift Card')]")
 	WebElement CorporateGiftCard_Element;
-	
-	@FindBy(xpath="//input[@name=\"senderName\"]")
+
+	@FindBy(xpath = "//input[@name=\"senderName\"]")
 	WebElement SendersName_Element;
-	
-	@FindBy(xpath="//input[@name=\"senderMobileNo\"]")
+
+	@FindBy(xpath = "//input[@name=\"senderMobileNo\"]")
 	WebElement SendersMobileNum_Element;
-	
-	@FindBy(xpath="//input[@name=\"senderEmailId\"]")
+
+	@FindBy(xpath = "//input[@name=\"senderEmailId\"]")
 	WebElement sendersEmailiD_Element;
-	
-	@FindBy(xpath="//button")
+
+	@FindBy(xpath = "//button")
 	WebElement buyNowButton_Element;
 	
+	@FindBy(xpath = "//span[text()='Payment options']")
+	WebElement paymentOptionLabel_Element;
+
 	public void shiftDriverToCardsPage() {
 		try {
-		logger.log(Status.INFO, "Shifting driver focus to the Gift cards page ");
-		for(String winHandle : driver.getWindowHandles()){
-			   driver.switchTo().window(winHandle);
-			   
+			logger.log(Status.INFO, "Shifting driver focus to the Gift cards page ");
+			for (String winHandle : driver.getWindowHandles()) {
+				driver.switchTo().window(winHandle);
+
 			}
-		}catch (Exception e) {
+		} catch (Exception e) {
 			reportFail(e.getMessage());
 		}
 	}
-	
+
 	public void verifyGiftCardsPageURL() {
 		try {
 			String ExpectedURL = prop.getProperty("giftCardPageURL");
@@ -65,64 +72,81 @@ public class GiftCardsPage extends PagesBaseClass{
 		}
 	}
 
-	/******************** Select corporate Gift Card******************************/
+	/******************** Select corporate Gift Card ******************************/
 	public void clickOnCorporateGiftCard() {
 		try {
-			JavascriptExecutor executor = (JavascriptExecutor)driver;
+			JavascriptExecutor executor = (JavascriptExecutor) driver;
 			executor.executeScript("arguments[0].click();", CorporateGiftCard_Element);
 			logger.log(Status.PASS, "Clicked on corporate Gift Card");
 		} catch (Exception e) {
 			reportFail(e.getMessage());
 		}
-		
+
 	}
-	
-	/********Enter  Senders Name in corporate gift card************/
+
+	/******** Enter Senders Name in corporate gift card ************/
 	public void enterSendersName() {
 		try {
-			
-			 SendersName_Element.sendKeys("Pushkar");
-			 
+
+			SendersName_Element.sendKeys("Pushkar");
+
 			logger.log(Status.PASS, "Entered the Senders Name");
 		} catch (Exception e) {
 			reportFail(e.getMessage());
 		}
 	}
-	
-	/***********Enter Mobile No in Corporate Gift Card**********/
+
+	/*********** Enter Mobile No in Corporate Gift Card **********/
 	public void enterMobileNo() {
 		try {
-			
+
 			SendersMobileNum_Element.sendKeys("8271987994");
-			 
 			logger.log(Status.PASS, "Entered the Mobile No");
 		} catch (Exception e) {
 			reportFail(e.getMessage());
 		}
 	}
-	/********Enter Valid Email Id in Corporate Gift Card********/
+
+	/******** Enter Valid Email Id in Corporate Gift Card ********/
 	public void enterValidEmailId() {
 		try {
-			
+
 			sendersEmailiD_Element.sendKeys("singhpushkar108@gmail.com");
-			 
+
 			logger.log(Status.PASS, "Entered the Valid Email ID");
 		} catch (Exception e) {
 			reportFail(e.getMessage());
 		}
 	}
-	
-	/*******Click on Buy Now Button*******************/
+
+	/******* Click on Buy Now Button *******************/
 	public void clickonBuyNowButton() {
- try {
-			
-	  buyNowButton_Element.click();
-			 
+		try {
+
+			buyNowButton_Element.click();
 			logger.log(Status.PASS, "Clicked on Buy Now Button");
 		} catch (Exception e) {
 			reportFail(e.getMessage());
 		}
 	}
-			
-			
+	
+	/*************** Verify payment page *******************/
+	public boolean isRedirectedToPaymentPage() {
+		try {
+			WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+			wait.until(ExpectedConditions.visibilityOf(paymentOptionLabel_Element));
+			String str = paymentOptionLabel_Element.getText();
+			if(str.equals("Payment options")) {
+				logger.log(Status.PASS, "Redirected to the payment page.");
+				return true;
+			}else {
+				logger.log(Status.INFO, "Redirected to the payment page Failled .");
+				return false;
+			}
+		} catch (Exception e) {
+			reportFail(e.getMessage());
+		}
+		return false;
+	}
+
 }
--- src/test/java/testCases/FillingCorporateGiftCardCredentials/SendersDetailsWithVaildEmailTest.java
+++ src/test/java/testCases/FillingCorporateGiftCardCredentials/SendersDetailsWithVaildEmailTest.java
@@ -1,33 +1,33 @@
 package testCases.FillingCorporateGiftCardCredentials;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import BaseClasses.BaseTestClass;
 import BaseClasses.PagesBaseClass;
 
-
-public class SendersDetailsWithVaildEmailTest extends BaseTestClass{
+public class SendersDetailsWithVaildEmailTest extends BaseTestClass {
 
 	@Test
-	        public void sendersDetailsWithValidEmail() throws InterruptedException  {
-		  logger  = report.createTest("Senders Details With Valid Email");
-			PagesBaseClass objPagesBaseClass = new PagesBaseClass(driver, logger);
-			landingPage = objPagesBaseClass.OpenApplication();
-			landingPage.closeLoginPopUp();
-			String currWin = driver.getWindowHandle();
-			giftCardsPage = landingPage.getGiftCardsPage();
-			giftCardsPage.shiftDriverToCardsPage();
-			giftCardsPage.verifyGiftCardsPageURL();
-			giftCardsPage.clickOnCorporateGiftCard();
-			
-			giftCardsPage.enterSendersName();
-			giftCardsPage.enterMobileNo();
-			giftCardsPage.enterValidEmailId();
-			giftCardsPage.clickonBuyNowButton();
-			Thread.sleep(4000);
-			driver.close();
-		//	driver.switchTo().window(currWin);
-		}
-		
+	public void sendersDetailsWithValidEmail() throws InterruptedException {
+		logger = report.createTest("Senders Details With Valid Email");
+		PagesBaseClass objPagesBaseClass = new PagesBaseClass(driver, logger);
+		landingPage = objPagesBaseClass.OpenApplication();
+		landingPage.closeLoginPopUp();
+		String currWin = driver.getWindowHandle();
+		giftCardsPage = landingPage.getGiftCardsPage();
+		giftCardsPage.shiftDriverToCardsPage();
+		giftCardsPage.verifyGiftCardsPageURL();
+		giftCardsPage.clickOnCorporateGiftCard();
+
+		giftCardsPage.enterSendersName();
+		giftCardsPage.enterMobileNo();
+		giftCardsPage.enterValidEmailId();
+		giftCardsPage.clickonBuyNowButton();
+		boolean flag = giftCardsPage.isRedirectedToPaymentPage();
+		Assert.assertEquals(flag, true);
+		driver.close();
+		driver.switchTo().window(currWin);
 	}
 
+}
